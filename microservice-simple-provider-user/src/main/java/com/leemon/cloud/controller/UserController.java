9292bf678790a90a44528379b9bdab7f13a246b8
limenglong <limenglong@juxinli.com>
2019-06-12 17:33:47 +0800

    SpringCloud Ribbon自定义负载均衡策略

--- microservice-simple-provider-user/src/main/java/com/leemon/cloud/controller/UserController.java
+++ microservice-simple-provider-user/src/main/java/com/leemon/cloud/controller/UserController.java
@@ -2,7 +2,10 @@ package com.leemon.cloud.controller;
 
 import com.leemon.cloud.entity.User;
 import com.leemon.cloud.repository.UserRepository;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +20,16 @@ import java.util.Optional;
  **/
 @RestController
 @RequestMapping("/users")
+@Configuration
+@Log
 public class UserController {
 
+    @Value("${server.port}")
+    private String port;
+
+
     private final UserRepository userRepository;
+
 
     @Autowired
     public UserController(UserRepository userRepository) {
@@ -29,6 +39,7 @@ public class UserController {
 
     @GetMapping(value = "/{id}", produces = "application/json;charset=UTF-8")
     public Optional<User> findById(@PathVariable("id") Long id) {
+        log.info("端口：" + port + " 的微服务执行了查询");
         return userRepository.findById(id);
     }
 }
