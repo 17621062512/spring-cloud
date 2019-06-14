20d7a84df955a6b37b9d3d216e9045a3372beb69
limenglong <limenglong@juxinli.com>
2019-06-14 15:49:31 +0800

    SpringCloud Feign使用hystrix监控及可视化监控

--- microservice-consumer-movie-feign-hystrix/src/main/java/com/leemon/hystrix/FeignHystrixApplication.java
+++ microservice-consumer-movie-feign-hystrix/src/main/java/com/leemon/hystrix/FeignHystrixApplication.java
@@ -3,8 +3,11 @@ package com.leemon.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrixDashboard
+@EnableHystrix
 @EnableFeignClients
 @SpringBootApplication
 public class FeignHystrixApplication {
